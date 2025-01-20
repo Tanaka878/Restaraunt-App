4ecdf69b1f7d5d54bb85a88ee7f051a1d77c4b57
Tanaka878 <musungaretanaka@gmail.com>
2025-01-20 15:33:52 +0200

    creating the details screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
-import { View, Text } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import DetailsScreen from './src/screens/DetailsScreen';
 
+const Stack = createNativeStackNavigator();
 
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+        <Stack.Screen name="Details" component={DetailsScreen} options={{animation:'slide_from_bottom'}} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+
   );
 };
 
